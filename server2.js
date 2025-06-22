7356fdb07819377250bb0469cb28615d1fc12e65
Visall666 <viisall59@gmail.com>
2025-06-22 19:01:27 +0700

    set condition to test api

--- server2.js
+++ server2.js
@@ -6,8 +6,17 @@ const users = [
     { id:2, name: "Choury KONG"}
 ];
 
-const server = createServer((req, res) =>{
-
+const server = createServer((req, res) => {
+    if(req.url === '/api/users' && req.method === 'GET') {
+        res.setHeader('Content-Type', 'application/json');
+        res.write(JSON.stringify(users));
+        res.end();
+    } else {
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 404;
+        res.write(JSON.stringify({ message: 'Route not found'}));
+        res.end();
+    }
 });
 
 server.listen(PORT, () => { 
